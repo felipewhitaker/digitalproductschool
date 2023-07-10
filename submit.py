f58026b9eeab5a1cc6a63af386cb2b815592a830
Felipe Whitaker <felipewhitaker+git@gmail.com>
2023-07-10 13:09:00 -0300

    Update `main.py`
    - Update `EC2` address
    - Add `predict` check

--- submit.py
+++ submit.py
@@ -2,18 +2,21 @@ import requests
 
 if __name__ == "__main__":
 
-    EC2_DYNAMIC_IP = "http://3.222.96.133:8000/"
+    EC2_IP = "http://107.22.134.151:8000/predict"
+    req = requests.post(EC2_IP, params={"year": 2021, "month": 1})
+    if not req.json().get("prediction", False):
+        raise ValueError("Prediction not found in response.")
 
     req = requests.post(
         "https://dps-challenge.netlify.app/.netlify/functions/api/challenge",
         json = dict(
             github = "https://github.com/felipewhitaker/digitalproductschool",
             email = "felipewhitaker+dps@gmail.com",
-            url = EC2_DYNAMIC_IP,
+            url = EC2_IP,
             notes = (
                 "Request must be made through `http` protocol, not `https`."
-                "Moreover, the application was deployed over a EC2 instance with"
-                "a dynamic IP address. Therefore, the IP address may change."
+                "The server is running on a free tier EC2 instance, so it may "
+                "take a few seconds to respond."
             )
         )
     )
